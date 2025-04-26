c39451b9da4d4cd6fb19e101788007eabe184cba
Adrian PK <adrianpk@adrianpk.com>
2025-04-26 19:52:17 +0200

    Implement user handlers

--- src/pulaplab/handlers/core.clj
+++ src/pulaplab/handlers/core.clj
@@ -1,43 +1,45 @@
 (ns pulaplab.handlers.core
-  (:require [reitit.ring :as ring]
-            [ring.middleware.params :refer [wrap-params]]
-            [pulaplab.auth.handlers :as auth-web]))
+  (:require
+   [reitit.ring :as ring]
+   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+   [pulaplab.auth.handlers :as auth-web]))
 
-(defn not-found-handler [_]
-  {:status 404
+(defn not-found-handler
+  "Return a 404 response when no route matches."
+  [_]
+  {:status  404
    :headers {"Content-Type" "text/plain"}
-   :body "Not found"})
+   :body    "Not found"})
 
 (def public-routes
   [["/" {:get (fn [_]
-                {:status 200
+                {:status  200
                  :headers {"Content-Type" "text/plain"}
-                 :body "Welcome to Pulap!"})}]
+                 :body    "Welcome to Pulap!"})}]
    ["/health" {:get (fn [_]
-                      {:status 200
+                      {:status  200
                        :headers {"Content-Type" "text/plain"}
-                       :body "OK"})}]])
+                       :body    "OK"})}]])
 
 (def private-routes
   [["/admin" {:get (fn [_]
-                     {:status 200
+                     {:status  200
                       :headers {"Content-Type" "text/plain"}
-                      :body "Admin dashboard (TODO auth)"})}]
-   ["/auth/list-users" {:get auth-web/list-users-handler}]
-   ["/auth/new-user" {:get auth-web/new-user-handler}]
-   ["/auth/create-user" {:post auth-web/create-user-handler}]
-   ["/auth/show-user" {:get auth-web/show-user-handler}]
-   ["/auth/edit-user" {:get auth-web/edit-user-handler}]
-   ["/auth/update-user" {:post auth-web/update-user-handler}]
-   ["/auth/delete-user" {:post auth-web/delete-user-handler}] ;; <-- agregar esta línea
-   ])
-
+                      :body    "Admin dashboard (TODO auth)"})}]
+   ["/auth/list-users"   {:get  auth-web/list-users-handler}]
+   ["/auth/new-user"     {:get  auth-web/new-user-handler}]
+   ["/auth/create-user"  {:post auth-web/create-user-handler}]
+   ["/auth/show-user"    {:get  auth-web/show-user-handler}]
+   ["/auth/edit-user"    {:get  auth-web/edit-user-handler}]
+   ["/auth/update-user"  {:post auth-web/update-user-handler}]
+   ["/auth/delete-user"  {:post auth-web/delete-user-handler}]])
 
 (def app
-  (ring/ring-handler
-   (ring/router
-    [["" {:middleware [wrap-params]}
-      public-routes
-      ["/private" private-routes]]])
-   (ring/create-default-handler
-    {:not-found not-found-handler})))
+  (-> (ring/ring-handler
+       (ring/router
+        [["" {:middleware []}
+          public-routes
+          ["/private" private-routes]]])
+       (ring/create-default-handler
+        {:not-found not-found-handler}))
+      (wrap-defaults site-defaults)))
