90af637c9e4a6c8cff27f1b4672f2a6912e74ee4
Adrian PK <adrianpk@adrianpk.com>
2025-05-07 09:16:54 +0200

    Improve styles

--- src/pulaplab/ui/styles.clj
+++ src/pulaplab/ui/styles.clj
@@ -16,7 +16,7 @@
    :button-delete "inline-flex bg-red-500 text-white px-4 py-2 rounded justify-center items-center"
    :button-new "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
    :form-label "block text-sm font-medium text-gray-700"
-   :form-input "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+   :form-input "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm px-3 py-2"
    :header-link "text-white"
    :menu-list "flex space-x-4"
    :cancel-button "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
